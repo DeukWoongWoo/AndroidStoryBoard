b02d4deaf28f59efc7c4c08c6c05a8ef4be98f93
ojg0508 <ojg508@gmail.com>
2016-01-15 20:33:39 +0900

    addObject에서 DB쿼리 수정

--- Server/node_js_server/models/db.js
+++ Server/node_js_server/models/db.js
@@ -55,10 +55,9 @@ mysql.addAppUse = function(param){
      */
     mysql.query('SELECT app_info.app_num FROM user_info '
         + 'INNER JOIN app_info ON user_info.user_id = app_info.user_id '
-        + 'AND app_info.user_id = \''+ param.body.user_id + '\'',
-        function (error, result, fields) {
+        + 'AND app_info.user_id = \''+ param.body.user_id + '\''
+        , function (error, result, fields) {
         if (error) {
-            console.error('쿼리 문장에 오류가 있습니다.');
             console.error(error);
         } else {
             appUse.app_num = result[0].app_num;
@@ -97,19 +96,15 @@ mysql.addActivityUse = function(param){
      *  activity_use_info에 사용 정보를 기록하고
      *  activity_info에 total_time을 갱신
      */
-    mysql.query('SELECT * FROM user_info '
+    mysql.query('SELECT activity_info.activity_num FROM user_info '
         + 'INNER JOIN app_info ON user_info.user_id = app_info.user_id '
         + 'AND user_info.user_id = \'' + param.body.user_id + '\' '
         + 'INNER JOIN activity_info ON app_info.app_num = activity_info.app_num '
-        + 'AND activity_info.activity_name = \'' + param.body.activity_name + '\'',
-
-        function (error, result, fields) {
+        + 'AND activity_info.activity_name = \'' + param.body.activity_name + '\''
+        , function (error, result, fields) {
         if (error) {
-            console.error('쿼리 문장에 오류가 있습니다.');
             console.error(error);
         } else {
-            console.log(result);
-            console.log(result[0]);
             activityUse.activity_num = result[0].activity_num;
             mysql.query('insert into activity_use_info set ?', activityUse, throwError);
             mysql.updateActivityTotalTime(activityUse.activity_num, activityUse.during_time_start, activityUse.during_time_end);
@@ -135,21 +130,37 @@ mysql.addObject = function(param){
 }
 
 mysql.addObjectUse = function(param){
-
     var objectUse = {
         object_use_num:param.body.object_use_num,
         occur_time:param.body.occur_time,
         event_type:param.body.event_type,
-        object_num:param.body.object_num
+        object_num:null
     };
 
-    console.log(objectUse.occur_time);
-
-    mysql.query('insert into object_use_info set ?', objectUse, throwError);
+    /**
+     *  object_use_info에 사용 정보를 기록하고
+     *  object_info에 frequency을 갱신
+     */
+    mysql.query('SELECT object_info.object_num FROM user_info '
+        + 'INNER JOIN app_info ON user_info.user_id = app_info.user_id '
+        + 'AND user_info.user_id = ' + '\'' + param.body.user_id + '\' '
+        + 'INNER JOIN activity_info ON app_info.app_num = activity_info.app_num '
+        + 'AND activity_info.activity_name = \'' + param.body.activity_name + '\' '
+        + 'INNER JOIN object_info ON object_info.activity_num = activity_info.activity_num '
+        + 'AND object_info.object_name = \'' + param.body.object_name + '\''
+        , function (error, result, fields) {
+            if (error) {
+                console.error(error);
+            } else {
+                objectUse.object_num = result[0].object_num;
+                mysql.query('insert into object_use_info set ?', objectUse, throwError);
+                mysql.updateObjectFrequency(result[0].object_num);
+                //찾은 object_num에 사용횟수를 늘린다.
+            }
+        });
 }
 
-mysql.addObjectFrequency  = function(param){
-    var object_num = param.body.object_num;
+mysql.updateObjectFrequency  = function(object_num){
     mysql.query('update object_info set frequency=frequency+1 where object_num like ' + object_num, throwError);
 }
 
@@ -181,7 +192,7 @@ var throwError = function (error, result, fields) {
         console.error('쿼리 문장에 오류가 있습니다.');
         console.error(error);
     } else {
-        console.log(result);
+        //console.log(result);
     }
 }
 

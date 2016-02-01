989a4bdcae01c79e890c868cab4f0fcf3f02996e
DeukWoongWoo <suoong2@gmail.com>
2016-02-02 00:29:49 +0900

    test

--- Plugin/src/Analysis/Parser/XmlParser.java
+++ Plugin/src/Analysis/Parser/XmlParser.java
@@ -1,6 +1,8 @@
 package Analysis.Parser;
 
 import Analysis.Constant.ConstantEtc;
+import Analysis.Database.DatabaseManager.DatabaseManager;
+import Analysis.Database.DtatTransferObject.ManifestDTO;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.psi.PsiFile;
@@ -16,21 +18,25 @@ public class XmlParser implements FileParser {
 
     private XmlFile xmlFile;
 
+    private ManifestDTO manifestDTO = new ManifestDTO();
+
     public XmlParser(PsiFile psiFile){
         this.xmlFile = (XmlFile)psiFile;
     }
 
     @Override
     public void parsing() {
-        if(ConstantEtc.PROJECT_JAVA_PATH==null) {
-            makeProjectJavaPath();
-        }
+        makeProjectJavaPath();
+
         XmlTag xmlTag = xmlFile.getRootTag().findSubTags("application")[0];
+        activityTag(xmlTag);
 
 //        Stream<XmlTag> activityTag = Stream.of(xmlTag.findSubTags("activity"));
 //        activityTag.filter(subTag -> subTag.findSubTags("intent-filter").length != 0)
 //                .forEach(subTag -> System.out.println(subTag.findSubTags("intent-filter")[0].findSubTags("action")[0].getAttributeValue("android:name")));
+    }
 
+    private void activityTag(XmlTag xmlTag) {
         for(XmlTag subTag : xmlTag.findSubTags("activity")){
             System.out.println("subTag Content " + subTag.getAttributeValue("android:name"));
             if(subTag.findSubTags("intent-filter").length != 0){
@@ -49,12 +55,16 @@ public class XmlParser implements FileParser {
 
     private void makeProjectJavaPath() {
         String packagePath = xmlFile.getRootTag().getAttributeValue("package");
+
+        manifestDTO.setPackageName(packagePath);
+        DatabaseManager.getInstance().insertToManifest(table -> table.insertManifest(manifestDTO));
+
         String str[] = packagePath.split("\\.");
         String strPath = "/java";
         for(int i=0;i<str.length;i++)
             strPath += "/" + str[i];
         ConstantEtc.PROJECT_JAVA_PATH = strPath;
-        System.out.println("XmlParser javaPath : "+ ConstantEtc.PROJECT_JAVA_PATH);
-        Messages.showInfoMessage("javaPath : "+ ConstantEtc.PROJECT_JAVA_PATH,"XmlParser");
+//        System.out.println("XmlParser javaPath : "+ ConstantEtc.PROJECT_JAVA_PATH);
+//        Messages.showInfoMessage("javaPath : "+ ConstantEtc.PROJECT_JAVA_PATH,"XmlParser");
     }
 }

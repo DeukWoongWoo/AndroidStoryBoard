87a56387b92b491b917398ce08df9dc908a5a7fc
ojg0508 <ojg508@gmail.com>
2016-02-01 17:39:13 +0900

    더미 데이터 만들기 페이지 만듬

--- Server/node_js_server/routes/home.js
+++ Server/node_js_server/routes/home.js
@@ -5,38 +5,138 @@ var async = require('async');
 var fs = require('fs');
 var mkdirp = require('mkdirp');    // mkdirp 모듈있는 곳을 설정해주면됩니다.
 
-router.get('/ajax', function(req, res){
+router.get('/ajax', function (req, res) {
     console.log(req.body.name); // Ajax parameter data
-    db.query('select * from user_info', function(error, result){
+    db.query('select * from user_info', function (error, result) {
         res.send(result);
     });
 });
 
-router.get('/ajax/frequency', function(req, res){
+router.get('/ajax/frequency', function (req, res) {
     console.log(req.body.name); // Ajax parameter data
-    db.query('select object_info.object_frequency from object_info', function(error, result){
+    db.query('select object_info.object_frequency from object_info', function (error, result) {
         res.send(result);
     });
 });
 
-router.post('/date-search', function(req, res){
+router.get('/makedata', function (req, res) {
+    db.getAllUserID(null, function (err, result) {
+        res.render('makedata', {user: result});
+    });
+});
 
-    if(isDefined(req.body.start_date) && isDefined(req.body.end_date)){
+router.post('/makedata/app', function (req, res) {
+    db.getAppByUserId(req, function (err, result) {
+        if (err)res.send(err);
+        else res.render('makedataapp', {app: result, user_id: req.body.user_id});
+    });
+});
 
-    }else{
-        db.getUserAppObject(req, function(err, result){
+router.post('/makedata/app/add', function (req, res) {
+    db.addApp(req);
+    //TODO:새로고침
+});
+
+router.post('/makedata/activity', function (req, res) {
+    db.getActivityByAppNum(req, function (err, result) {
+        if (err)res.send(err);
+        else {
+            console.log('req body user_id : ', req.body);
+            res.render('makedataactivity', {activity: result, app_num: req.body.app_num, app_name: req.body.app_name});
+        }
+    });
+});
+
+router.post('/makedata/activity/add', function (req, res) {
+    var activity = {
+        activity_name: req.body.activity_name,
+        total_time: 0,
+        app_num: req.body.app_num
+    };
+    db.query('insert into activity_info set ?', activity, function (err) {
+        if (err)console.error(err);
+    });
+    //TODO:새로고침
+});
+
+
+router.post('/makedata/object', function (req, res) {
+    db.getObjectByActivityNum(req, function (err, result) {
+        if (err)res.send(err);
+        else {
+            res.render('makedataobject', {
+                object: result,
+                activity_num: req.body.activity_num,
+                activity_name: req.body.activity_name
+            });
+        }
+    });
+});
+
+router.post('/makedata/object/add', function (req, res) {
+    var object = {
+        object_name: req.body.object_name,
+        object_frequency: 0,
+        error_frequency: 0,
+        image_num: 1,
+        activity_num: req.body.activity_num
+    };
+    console.log(object.image_num);
+    db.query('insert into object_info set ?', object, function (err) {
+        if (err)console.error(err);
+    });
+    //TODO:새로고침
+});
+
+router.post('/makedata/object/use', function (req, res) {
+    console.log(req.body);
+    var use = {
+        object_num : req.body.object_num,
+        occur_time : '2016-02-01',
+        event_type : 'button'
+    }
+
+    db.query('insert into object_use_info set ?', use, function(err){
+        if(err)console.error(err);
+    });
+    db.query('update object_info set object_frequency=object_frequency+1 where object_num like ' + req.body.object_num, function(err){
+        if(err)console.error(err);
+    });
+    //TODO:새로고침
+});
+router.post('/makedata/object/err', function (req, res) {
+    console.log(req.body);
+    var use = {
+        object_num : req.body.object_num,
+        occur_time : '2016-02-01',
+    }
+
+    db.query('insert into error_use_info set ?', use, function(err){
+        if(err)console.error(err);
+    });
+    db.query('update object_info set error_frequency=error_frequency+1 where object_num like ' + req.body.object_num, function(err){
+        if(err)console.error(err);
+    });
+    //TODO:새로고침
+});
+
+router.post('/date-search', function (req, res) {
+    if (isDefined(req.body.start_date) && isDefined(req.body.end_date)) {
+
+    } else {
+        db.getUserAppObject(req, function (err, result) {
             console.log(req.body);
             console.log(err);
             console.log(result);
-            if(err) console.log(err);
+            if (err) console.log(err);
             else res.send(result);
         });
     }
 });
 
-router.post('/get/object_info', function(req, res){
-    db.getUserAppObject(req, function(err, result){
-        if(err) console.log(err);
+router.post('/get/object_info', function (req, res) {
+    db.getUserAppObject(req, function (err, result) {
+        if (err) console.log(err);
         else res.send(result);
     });
 });
@@ -89,14 +189,14 @@ function registerApp(req, res, callback) {
                         if (err) callback(err);
                         else callback(null);
                     });
-                },function (callback) {
+                }, function (callback) {
                     fileUpload(req, function (err) {
                         if (err) callback(err);
                         else callback(null);
                     });
-                },function (callback) {
+                }, function (callback) {
                     //parseJSON(req, function(err){
-                       callback(null);
+                    callback(null);
                     //});
                 }
             ], function (err) {

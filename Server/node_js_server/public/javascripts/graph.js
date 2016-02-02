6c5f44f8f3189f5f633839d8437d0cb4d03fe1d1
ojg0508 <ojg508@gmail.com>
2016-02-02 23:09:32 +0900

    graph, bar, axis

--- Server/node_js_server/public/javascripts/graph.js
+++ Server/node_js_server/public/javascripts/graph.js
@@ -1,128 +1,217 @@
 function Graph() {
     this.svgWidth = screen.width * 0.4;
     this.svgHeight = screen.height * 0.4;
-
     this.dataColor = 'teal';
     this.emptyDataColor = 'black';
+
+    this.ease = 'cubic';
     this.graphType = 'rect';
     this.selectTarget = null;
     this.rY = 1;
     this.maxY = null;
-    this.data = null;// = new Array();
-    this.dataName = null;// = new Array();
+    this.data = null;
+    this.dataName = null;
+
+    //this.dataObject = null;
     this.svg = null;
+    this.yAxis = null;
+    this.xAxis = null;
 }
 
-Graph.prototype.graphData = function(data){
+Graph.prototype.graphData = function (data) {
     this.data = data;
+    this.maxY = Math.max.apply(null, data);
     return this;
 }
 
-Graph.prototype.graphDataName = function(dataName){
+Graph.prototype.graphDataName = function (dataName) {
     this.dataName = dataName;
+    //this.dataObject.data(dataName)
+    //    .attr("class", function (d) {
+    //        return d + 'ABS-object-name'
+    //    })
+    //    .attr("number", function (d, i) {
+    //        return i;
+    //    });
     return this;
 }
 
-Graph.prototype.setWidth = function(width){
+Graph.prototype.setWidth = function (width) {
     this.svgWidth = width;
     return this;
 }
 
-Graph.prototype.setHeight = function(height){
+Graph.prototype.setHeight = function (height) {
     this.svgHeight = height;
     return this;
 }
 
-Graph.prototype.setDataColor = function(color){
+Graph.prototype.setDataColor = function (color) {
     this.dataColor = color;
     return this;
 }
 
-Graph.prototype.setType = function(type){
+Graph.prototype.setType = function (type) {
     this.graphType = type;
     return this;
 }
 
-Graph.prototype.setLocation = function(selectTarget){
+Graph.prototype.location = function (selectTarget) {
     this.selectTarget = selectTarget;
+    return this;
 }
 
-Graph.prototype.createGraph = function(){
+Graph.prototype.setEase = function (es) {
+    this.ease = es;
+    return this;
+}
+
+Graph.prototype.update = function (data) {
+    this.graphData(data);
+    this.drawBar();
+    this.drawYAxis();
+    this.drawXAxis();
+}
+
+Graph.prototype.draw = function () {
+    this.createGraph();
+
+    this.createBar();
+    this.drawBar();
+    this.animateBar("bounce-in");
+
+    this.createYAxis();
+    this.drawYAxis();
+    this.animateYAxis("bounce");
+
+    this.createXAxis();
+    this.drawXAxis();
+    this.animateXAxis("bounce");
+}
+
+Graph.prototype.createGraph = function () {
     var selectTarget = this.selectTarget;
-    var type = this.graphType;
     var width = this.svgWidth;
     var height = this.svgHeight;
-    var data = this.data;
-    this.maxY =  Math.max.apply(null, data);
-    this.rY = this.svgHeight / this.maxY;
 
+    $(selectTarget).empty();
     this.svg = d3.select(selectTarget).append("svg").attr("width", width).attr("height", height);
+}
+
+Graph.prototype.createBar = function () {
+    var type = this.graphType;
+    var data = this.data;
     this.svg.selectAll(type).data(data).enter().append(type);
     this.dataObject = this.svg.selectAll(type);
 }
 
-Graph.prototype.draw = function (){
-    this.drawBarGraph();
-    this.setDataName();
-    this.AnimateBarGraph();
-}
-
-Graph.prototype.AnimateBarGraph = function () {
-    var height = this.svgHeight;
+Graph.prototype.drawBar = function () {
     var data = this.data;
-    var maxY = Math.max.apply(null, data);
-    var rY = this.svgHeight / maxY;
+    var rY = this.rY = this.svgHeight / this.maxY;
+    var length = data.length;
 
-    this.dataObject.data(data).attr("y", height);
-    this.dataObject.data(data).transition().duration(1000).ease("bounce-in")
-        .attr("y", function(d){
-            return height - calY(d, rY) - 10;
-        });
-}
-
-function calY(d, rY){
-    return ((d * rY) * 0.8 + 1);
-}
-
-Graph.prototype.drawBarGraph = function(){
     var dataColor = this.dataColor;
     var emptyDataColor = this.emptyDataColor;
     var width = this.svgWidth;
     var height = this.svgHeight;
-    var data = this.data;
-    var length = data.length;
-    var rY = this.rY;
+    var es = this.ease;
 
     this.dataObject.data(data)
+        .attr("fill", function (d) {
+            if (d == 0) return emptyDataColor;
+            else return dataColor;
+        })
+        .transition().duration(1000).ease(es)
         .attr("x", function (d, i) {
             return i * (width / length) + 5;
         })
-        .attr("y", function(d){
-            return calY(d, height, rY);
+        .attr("y", function (d) {
+            return height - calY(d, rY) - 10;
         })
         .attr("width", width / length - ((width / length) * 0.1))
         .attr("height", function (d) {
             return calY(d, rY) + 5;
-        })
-        .attr("fill", function (d) {
-            if (d == 0) return emptyDataColor;
-            else return dataColor;
         });
 }
 
-Graph.prototype.setDataName = function(){
-    var dataName = this.dataName;
-    this.dataObject.data(dataName)
-        .attr("class", function (d) {
-            return d + 'ABS-object-name'
-        })
-        .attr("number", function (d, i) {
-            return i;
-        });
+Graph.prototype.animateBar = function (es) {
+    var height = this.svgHeight;
+    var data = this.data;
+    this.ease = es ? es : this.ease;
+
+    this.dataObject.data(data).attr("y", height);
+    this.drawBar();
 }
 
-Graph.prototype.setHightlight = function(){
-    console.log(this.selectTarget);
+Graph.prototype.createYAxis = function () {
+    var rY = this.rY = this.svgHeight / this.maxY;
+    var height = this.svgHeight;
+    var maxY = this.maxY;
+
+    this.yAxis = this.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (height - calY(maxY, rY) - 10) + ")");
+}
+
+Graph.prototype.drawYAxis = function () {
+    var height = this.svgHeight;
+    var maxY = this.maxY;
+    var rY = this.rY;
+    var es = this.ease;
+
+    var yScale = d3.scale.linear().domain([maxY, 0]).range([0, calY(maxY, rY) - 1]);
+    var yAxis = d3.svg.axis();
+    yAxis.scale(yScale).orient("right");
+
+    this.yAxis.transition().duration(500).ease(es)
+        .attr("transform", "translate(0," + (height - calY(maxY, rY) - 10) + ")")
+        .call(yAxis);
+}
+
+Graph.prototype.animateYAxis = function (es) {
+    var height = this.svgHeight;
+    this.ease = es ? es : this.ease;
+
+    this.yAxis.attr("transform", "translate(0," + height / 2 + ")");
+    this.drawYAxis();
+}
+
+Graph.prototype.createXAxis = function () {
+    var height = this.svgHeight;
+
+    this.xAxis = this.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")");
+}
+
+Graph.prototype.drawXAxis = function () {
+    var height = this.svgHeight;
+    var width = this.svgWidth;
+    var es = this.ease;
+    var xAxis = d3.svg.axis();
+    var xScale = d3.scale.linear().domain([0, 0]).range([0, width]);
+
+    xAxis.scale(xScale).orient("top");
+    this.xAxis.transition().duration(500).ease(es)
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+}
+
+Graph.prototype.animateXAxis = function (es) {
+    var height = this.svgHeight;
+    var width = this.svgWidth;
+    this.ease = es ? es : this.ease;
+
+    this.xAxis.attr("transform", "translate(" + (-width) +  ", " + height + ")");
+    this.drawXAxis();
+}
+
+function calY(d, rY) {
+    return ((d * rY) * 0.8 + 1);
+}
+
+
+Graph.prototype.setHightlight = function () {
     var data = this.data;
     var dataName = this.dataName;
     $('.graph').children('svg').children('rect').mouseover(function () {
@@ -137,10 +226,6 @@ Graph.prototype.setHightlight = function(){
         $('.rect-info').css("visibility", "visible");
         rectInfo.children('#rect-object-name').text(dataName[number]);
         rectInfo.children('#rect-use-frequency').text(data[number]);
-        //rectInfo.children('#rect-err-frequency').text(data[number].error_frequency);
-        //rectInfo.children('#rect-activity-name').text(data[number].activity_name);
-        //rectInfo.children('#rect-activity-time').text(data[number].total_time);
-
     }).mouseout(function () {
 
     });
@@ -150,38 +235,6 @@ Graph.prototype.setHightlight = function(){
  *
  */
 
-Graph.prototype.drawYAxis = function () {
-    var height = this.svgHeight;
-    var maxY = this.maxY;
-    var rY = this.rY;
-
-    var yScale = d3.scale.linear().domain([maxY, 0]).range([0, calY(maxY, rY)]);
-    var yAxis = d3.svg.axis();
-    yAxis.scale(yScale).orient("right");
-    this.svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + (h - ((calY(maxY, rY) + 1) - 10) + ")"))
-        .call(yAxis);
-
-    return this;
-}
-
-Graph.prototype.drawXAxis = function () {
-    var h = this.h;
-    var w = this.w;
-
-    var xAxis = d3.svg.axis();
-    var xScale = d3.scale.linear().domain([0, 0]).range([0, w]);
-    xAxis.scale(xScale).orient("top");
-    this.svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + h + ")")
-        .call(xAxis);
-}
-
-/**
- *
- */
 
 Graph.prototype.drawBarGraph__ = function (divId, data, frequency) {
     var w = this.w;
@@ -284,13 +337,13 @@ Graph.prototype.drawLineGraph = function (divId, data, frequency) {
             return i * (w / length) + 5;
         })
         .attr("y1", function (d) {
-            return h/2;
+            return h / 2;
         })
         .attr("x2", function (d, i) {
-            return (i+1) * (w / length) + 5;
+            return (i + 1) * (w / length) + 5;
         })
         .attr("y2", function (d, i) {
-            return h/2;
+            return h / 2;
         })
         .style("stroke", "rgb(6,120,155)");
 
@@ -305,7 +358,7 @@ Graph.prototype.drawLineGraph = function (divId, data, frequency) {
             return (i + 1) * (w / length) + 5;
         })
         .attr("y2", function (d, i) {
-            if(i + 1 < length)
+            if (i + 1 < length)
                 return h - ((frequency[i + 1] * rY) * 0.8 + 1) - 10;
             else return h - (d * rY * 0.8 + 1) - 10;
         });
@@ -347,7 +400,7 @@ Graph.prototype.updateLineGraph = function (data, frequency) {
             return (i + 1) * (w / length) + 5;
         })
         .attr("y2", function (d, i) {
-            if(i + 1 < length)
+            if (i + 1 < length)
                 return h - ((frequency[i + 1] * rY) * 0.8 + 1) - 10;
             else return h - (d * rY * 0.8 + 1) - 10;
         });
@@ -381,37 +434,37 @@ Graph.prototype.setAnimation = function (target, data) {
     var ret = this.svg.selectAll(target).data(data).transition().duration(1000).ease("bounce-out");
     return ret;
 }
-
-Graph.prototype.drawYAxis = function () {
-    var h = this.h;
-
-    var maxY = this.maxY;
-    var rY = this.rY;
-
-    var yScale = d3.scale.linear().domain([maxY, 0]).range([0, (maxY * rY * 0.8)]);
-    var yAxis = d3.svg.axis();
-    yAxis.scale(yScale).orient("right");
-    this.svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + (h - ((maxY * rY) * 0.8 + 1) - 10) + ")")
-        .call(yAxis);
-
-    return this;
-}
-
-Graph.prototype.drawXAxis = function () {
-    var h = this.h;
-    var w = this.w;
-
-    var xAxis = d3.svg.axis();
-    var xScale = d3.scale.linear().domain([0, 0]).range([0, w]);
-    xAxis.scale(xScale).orient("top");
-    this.svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + h + ")")
-        .call(xAxis);
-
-}
+//
+//Graph.prototype.drawYAxis = function () {
+//    var h = this.h;
+//
+//    var maxY = this.maxY;
+//    var rY = this.rY;
+//
+//    var yScale = d3.scale.linear().domain([maxY, 0]).range([0, (maxY * rY * 0.8)]);
+//    var yAxis = d3.svg.axis();
+//    yAxis.scale(yScale).orient("right");
+//    this.svg.append("g")
+//        .attr("class", "axis")
+//        .attr("transform", "translate(0," + (h - ((maxY * rY) * 0.8 + 1) - 10) + ")")
+//        .call(yAxis);
+//
+//    return this;
+//}
+//
+//Graph.prototype.drawXAxis = function () {
+//    var h = this.h;
+//    var w = this.w;
+//
+//    var xAxis = d3.svg.axis();
+//    var xScale = d3.scale.linear().domain([0, 0]).range([0, w]);
+//    xAxis.scale(xScale).orient("top");
+//    this.svg.append("g")
+//        .attr("class", "axis")
+//        .attr("transform", "translate(0," + h + ")")
+//        .call(xAxis);
+//
+//}
 
 Graph.prototype.setColor = function (color) {
     this.barColor = color;

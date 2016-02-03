0cde0826655aa0c71b6d86aa5071379796568921
ojg0508 <ojg508@gmail.com>
2016-02-03 14:49:49 +0900

    축에 대한 함수 추출 하이라이트 예정

--- Server/node_js_server/public/javascripts/graph/Graph.js
+++ Server/node_js_server/public/javascripts/graph/Graph.js
@@ -18,6 +18,24 @@ function Graph() {
     this.xAxis = null;
 
     this.gType = null;
+
+    this.xAxisData = {
+        orient: 'top',
+        position: {x: this.svgWidth/2, y: this.svgHeight/2},
+        range: {start: -this.svgWidth, end: this.svgWidth},
+        domain: {start: -this.svgWidth, end: this.svgWidth}
+    };
+
+
+    this.yAxisData = {
+        orient: 'right',
+        position: {x: this.svgWidth/2, y: this.svgHeight/2},
+        range: {start: -this.svgHeight, end: this.svgHeight},
+        domain: {start: -this.svgHeight, end: this.svgHeight}
+        //position: {x: 0, y: height - calY(maxY, rY) - 10},
+        //range: {start: 0, end: calY(maxY, rY) - 1},
+        //domain: {start: maxY, end: 0}
+    };
 }
 
 Graph.prototype = {
@@ -50,6 +68,7 @@ Graph.prototype = {
     },
 
     location: function (selectTarget) {
+        $(this.selectTarget).empty();
         this.selectTarget = selectTarget;
         return this;
     },
@@ -86,21 +105,27 @@ Graph.prototype = {
     update: function (data) {
         //this.setEase("circle");
         this.setGraphType(this.gType);
+        this.updateAxis();
 
         return this.gType.draw();
     },
 
     draw: function () {
-        return this.gType.draw();
+        this.gType.draw()
+        this.drawAxis();
+
+        return this.gType;
     },
 
     create: function () {
-        this.gType.create();
-        this.axis();
+        this.gType.create().draw().animate();
+        this.axis().animateAxis();
+
         return this.gType;
     },
 
     animate: function () {
+        this.animateAxis();
         return this.gType.animate('bounce');
     },
 
@@ -115,41 +140,82 @@ Graph.prototype = {
         return this;
     },
 
-    axis : function(){
-        var height = this.svgHeight;
-        var width = this.svgWidth;
-        var maxY = this.maxY;
-        var rY = this.rY;
-        var axisData;
+    setXAxisData : function(axisData){
+        this.xAxisData = axisData;
+    },
 
+    setYAxisData : function(axisData){
+        this.yAxisData = axisData;
+    },
+
+    axis : function(){
         this.xAxis = this.createAxis();
         this.yAxis = this.createAxis();
+        this.setAxisData();
 
-        axisData = {
+        return this;
+    },
+
+    updateAxis : function(){
+        this.setAxisData();
+
+        this.drawAxis(this.xAxis, this.xAxisData);
+        this.drawAxis(this.yAxis, this.yAxisData);
+    },
+
+    setAxisData : function(){
+        this.setXAxisData({
             orient: 'top',
-            position: {x: 0, y: height},
-            range: {start: 0, end: width},
+            position: {x: 0, y: this.svgHeight},
+            range: {start: 0, end: this.svgWidth},
             domain: {start: 0, end: 0}
-        };
-        this.drawAxis(this.xAxis, axisData);
+        });
 
-        axisData = {
+        this.setYAxisData({
             orient: 'right',
-            position: {x: 0, y: height - calY(maxY, rY) - 10},
-            range: {start: 0, end: calY(maxY, rY) - 1},
-            domain: {start: maxY, end: 0}
-        };
-        this.drawAxis(this.yAxis, axisData);
+            position: {x: 0, y: this.svgHeight - calY(this.maxY, this.rY) - 10},
+            range: {start: 0, end: calY(this.maxY, this.rY) - 1},
+            domain: {start: this.maxY, end: 0}
+        });
     },
 
     createAxis: function () {
         return this.svg.append("g").attr("class", "axis");
     },
+
     drawAxis: function (axis, axisData) {
         ////축 데이터
         var scale = d3.scale.linear().domain([axisData.domain.start, axisData.domain.end]).range([axisData.range.start, axisData.range.end]);
         axis.transition().duration(500).ease(this.ease)
             .attr("transform", "translate(" + axisData.position.x + "," + axisData.position.y + ")")
+            .call(d3.svg.axis().scale(scale).orient(axisData.orient));
+    },
+
+    animateAxis : function(){
+        this.middleAxis();
+
+        this.drawAxis(this.xAxis, this.xAxisData);
+        this.drawAxis(this.yAxis, this.yAxisData);
+    },
+
+    middleAxis : function(){
+        this.drawAxisWithoutAni(this.xAxis, {
+            orient: 'top',
+            position: {x: this.svgWidth/2, y: this.svgHeight/2},
+            range: {start: -this.svgWidth, end: this.svgWidth},
+            domain: {start: -this.svgWidth, end: this.svgWidth}
+        });
+        this.drawAxisWithoutAni(this.yAxis, {
+            orient: 'right',
+            position: {x: this.svgWidth/2, y: this.svgHeight/2},
+            range: {start: -this.svgHeight, end: this.svgHeight},
+            domain: {start: -this.svgHeight, end: this.svgHeight}
+        });
+    },
+
+    drawAxisWithoutAni : function (axis, axisData) {
+        var scale = d3.scale.linear().domain([axisData.domain.start, axisData.domain.end]).range([axisData.range.start, axisData.range.end]);
+        axis.attr("transform", "translate(" + axisData.position.x + "," + axisData.position.y + ")")
             .call(d3.svg.axis().scale(scale).orient(axisData.orient));
     }
 

29f0b36924ce28f03aa023c53cca1f093d5897df
unknown <>
2016-02-19 15:17:49 +0900

    스토리보드 activity 창 새로 생성시 보내는 json 형식에 대해 생각함

--- Plugin/src/GUI/StoryBoard/Constant.java
+++ Plugin/src/GUI/StoryBoard/Constant.java
@@ -6,8 +6,37 @@ import java.awt.*;
  * Created by 우철 on 2016-02-11.
  */
 public interface Constant  {
-    int activitySize_X = 192;
-    int activitySize_Y = 320;
-    String FILE_ROUTE ="C:\\Users\\우철\\IdeaProjects\\cal\\jsontest_+layout.txt";
+    int activitySize_X = 300;
+    int activitySize_Y = 500;
+     String FILE_ROUTE ="C:/Users/우철/Documents/AndroidStoryBoard/Plugin/jsonfile.txt";
     Dimension MENUBUTTONSIZE = new Dimension(50,50);
+    int SCALE_SHAPE = 1;
+
+
+    class JsonFileStruct{
+        String activity;
+        String parent_object;
+        String pri_activity;
+        String pri_parent_object;
+        String name;
+        int x;
+        int y;
+        int width;
+        int height;
+        String type;
+
+        public JsonFileStruct(){
+            activity = "포함하고 있는 activity";
+            parent_object = "포함하고 있는 object";
+            pri_activity = "포함했던 activity";
+            pri_parent_object= "포함 했던 object";
+            name = "id 혹은 이름";
+            x = 0;
+            y= 0;
+            width =0;
+            height =0;
+            type ="type";
+        }
+    }
+
 }
--- Plugin/src/GUI/StoryBoard/storyBoard.java
+++ Plugin/src/GUI/StoryBoard/storyBoard.java
@@ -6,14 +6,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.awt.geom.AffineTransform;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -30,9 +30,10 @@ public class storyBoard extends JPanel {
     JSONArray activityArrayData;
     private Point prePoint;
     private int zoom = 0;
-
+    private String appName;
     HashMap <String, Activity> activity_list = new HashMap();
 
+    // 생성자
     public storyBoard() throws IOException {
         abc = new JButton("push");
         li = new listner();
@@ -89,39 +90,18 @@ public class storyBoard extends JPanel {
         drawActivity();
         System.out.println("StroyBoard Add");
     }
+    public void repaint_window()
+    {
+        revalidate();       // 무효화 선언된 화면을 알려줌
+        repaint();
+    }
 
-
+    //리스너
     class listner implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
             if(e.getSource()==abc) {
-                String resultStr = null;
-
-                resultStr = JOptionPane.showInputDialog("이름을 입력해 주세요.");
-                if(resultStr==null )
-                {
-
-                }
-                else
-                {
-                    if(activity_list.containsKey(resultStr))
-                    {
-                        JOptionPane.showMessageDialog(null, "이미 중복되어있는 ID 값입니다.");
-                    }
-                    else
-                    {
-
-                        Activity a = new Activity(resultStr, activity_list);
-                        a.setOverbearing(true);
-                        activity_list.put(resultStr, a);
-                        jpan.add(a);
-                        //a.setBounds(a.nowX, a.nowY, Constant.activitySize_X, Constant.activitySize_Y);
-
-                    }
-
-
-
-                }
+                NewWindow a = new  NewWindow();
 
                 revalidate();       // 무효화 선언된 화면을 알려줌
                 repaint();          // 다시 그려준다.
@@ -131,13 +111,19 @@ public class storyBoard extends JPanel {
     }
 
     //JObjectRoute 파일 경로. text 파일을 읽어온다.
-    JSONObject parserJObject(String JObjectRoute) throws IOException {
-        JSONParser parser =new JSONParser();
+    JSONObject parserJObject(String JObjectRoute) {
+        JSONParser par =new JSONParser();
+
         try {
-            Object obj = parser.parse(new FileReader(JObjectRoute));
+            System.out.println("JsonObjectRoute : " + JObjectRoute);
+            FileReader in = new FileReader(JObjectRoute);
+            System.out.println(in);
+            Object obj = par.parse(in);
+            System.out.println(obj);
             return (JSONObject)obj;
         }
         catch(Exception e){
+            System.out.println("error : "  + e);
             return null;
         }
 
@@ -154,10 +140,13 @@ public class storyBoard extends JPanel {
         }
         activity_list.clear();   // 현재까지 key 값들 모두 제거
     }
-
-    public void drawActivity() throws IOException {
+    // JSON 파일을 받은 것을 전부 그려준다.
+    public void drawActivity() {
         removeAllActivity();
         jobjRoot = parserJObject(Constant.FILE_ROUTE);
+        System.out.println(jobjRoot);
+
+        setAppName((String)jobjRoot.get("appName"));
         activityArrayData = (JSONArray)jobjRoot.get("activity");
 
         // 가지고 있는 액티비티를 만든다.
@@ -173,5 +162,190 @@ public class storyBoard extends JPanel {
         revalidate();       // 무효화 선언된 화면을 알려줌
         repaint();          // 다시 그려준다.
     }
+
+
+    class NewWindow extends JFrame{
+
+        JLabel name_label = new JLabel("name :");
+        JLabel id_label = new JLabel("id :");
+        JTextField id_field = new JTextField();
+        JTextField name_field =new JTextField();
+        JButton okbutton = new JButton("OK");
+
+        public NewWindow() {
+
+            int standard_x =Constant.activitySize_X;
+            int standard_y =Constant.activitySize_Y;
+            int standard_scale = standard_y/250;
+
+            this.setSize(standard_x+standard_x/10, standard_y/3);          //창 사이즈
+            this.setUndecorated(true);      //title bar 제거
+            this.setLocation(100,100);
+            this.setVisible(true);
+            this.setLayout(null);   // 자유 레이아웃
+            this.getRootPane().setBorder(new LineBorder(Color.black));  //테두리 설정
+            System.out.println("make new window");
+
+
+            okbutton.setMargin(new Insets(0, 0, 0, 0));
+
+            id_field.setFont(new Font("Serif", Font.PLAIN, standard_scale*18 ));
+            name_field.setFont(new Font("Serif", Font.PLAIN, standard_scale*18 ));
+            id_label.setFont(new Font("Serif", Font.PLAIN, standard_scale*18 ));
+            name_label.setFont(new Font("Serif", Font.PLAIN, standard_scale*18 ));
+
+
+            name_label.setLocation(standard_scale*5, standard_scale*5);
+            id_label.setLocation(standard_scale*5, standard_scale*30);
+            name_field.setLocation(standard_scale*55, standard_scale*5);
+            id_field.setLocation(standard_scale*55, standard_scale*30);
+            okbutton.setLocation(standard_scale*100, standard_scale*55);
+
+            name_label.setSize(standard_scale*50, standard_scale*20);
+            id_label.setSize(standard_scale*50, standard_scale*20);
+            name_field.setSize(standard_scale*100, standard_scale*20);
+            id_field.setSize(standard_scale*100, standard_scale*20);
+            okbutton.setSize(standard_scale*50, standard_scale*20);
+
+            this.add(name_label);
+            this.add(id_label);
+
+            this.add(name_field);
+            this.add(id_field);
+            this.add(okbutton);
+
+            this.addWindowFocusListener(new WindowFocusListener() {
+                @Override
+                public void windowGainedFocus(WindowEvent e) {
+                }
+
+                @Override
+                public void windowLostFocus(WindowEvent e) {
+                    dispose();
+                }
+            });
+
+            okbutton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    result(id_field.getText(), activity_list);
+                    dispose();
+                }
+            });
+
+            id_field.addKeyListener(new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        result(id_field.getText(), activity_list);
+                        dispose();
+                    }
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+
+                }
+            });
+            name_field.addKeyListener(new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        result(id_field.getText(), activity_list);
+                        dispose();
+                    }
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+
+                }
+            });
+
+        }
+
+
+        public void result(String resultStr ,HashMap<String, Activity> list)
+        {
+            if(list.containsKey(resultStr)==true)
+            {
+                JOptionPane.showMessageDialog(null, resultStr+"는 이미 중복되어있는 ID 값입니다.");
+            }
+            else {
+                System.out.println(resultStr +"Activity 가 생성되었습니다.");
+                Constant.JsonFileStruct file = new Constant.JsonFileStruct();
+                JSONObject newjobj = new JSONObject();
+
+
+                Activity a = new Activity(resultStr, list);
+                a.setOverbearing(true);
+                list.put(resultStr, a);
+                jpan.add(a);
+
+                file.height=a.getActivity_height();
+                file.width =a.getActivity_width();
+                file.x = a.getActivity_position().x;
+                file.y = a.getActivity_position().y;
+                file.name=a.getName();
+                file.type=a.getType();
+
+
+                newjobj =makeSendData(newjobj, file, "new");
+                System.out.println(newjobj);
+                repaint_window();
+            }
+        }
+    }
+
+    // json 형식을 만들어준다.
+
+    public JSONObject makeSendData(JSONObject jobj, Constant.JsonFileStruct file, String newandfix)
+    {
+        JSONObject temp = jobj;
+        JSONObject newone = new JSONObject();
+        JSONObject fixone = new JSONObject();
+        temp.put("status", newandfix);
+            newone.put("activity", file.activity);
+            newone.put("parent object", file.parent_object);
+            newone.put("name", file.name);
+            newone.put("x", file.x);
+            newone.put("y", file.y);
+            newone.put("width", file.width);
+            newone.put("height", file.height);
+            newone.put("type",file.type);
+        temp.put("new", newone);
+            fixone.put("activity", file.activity);
+            fixone.put("parent object", file.parent_object);
+            fixone.put("pri activity", file.pri_activity);
+            fixone.put("pri parent object", file.pri_parent_object);
+            fixone.put("name", file.name);
+            fixone.put("x", file.x);
+            fixone.put("y", file.y);
+            fixone.put("width", file.width);
+            fixone.put("height", file.height);
+            fixone.put("type",file.type);
+        temp.put("fix", fixone);
+
+        return temp;
+    }
+
+    //-----------private 접근 함수--------------------------
+    public String getAppName() {
+        return appName;
+    }
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
 
 }

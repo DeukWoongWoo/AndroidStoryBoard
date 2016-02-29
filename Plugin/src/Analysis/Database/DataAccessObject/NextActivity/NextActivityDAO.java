6ceaf37d90154e653d7c8d447d9c506eeba0bf7b
DeukWoongWoo <suoong2@gmail.com>
2016-03-01 00:26:25 +0900

    NextActivity 여러개 추가 수정

--- Plugin/src/Analysis/Database/DataAccessObject/NextActivity/NextActivityDAO.java
+++ Plugin/src/Analysis/Database/DataAccessObject/NextActivity/NextActivityDAO.java
@@ -2,11 +2,13 @@ package Analysis.Database.DataAccessObject.NextActivity;
 
 import Analysis.Database.DtatTransferObject.NextActivityDTO;
 
+import java.util.ArrayList;
+
 /**
  * Created by woong on 2016-02-29.
  */
 public interface NextActivityDAO {
     public void create();
     public void insert(NextActivityDTO nextActivityDTO);
-    public NextActivityDTO select(String... col);
+    public ArrayList<NextActivityDTO> select(String... col);
 }
--- Plugin/src/Analysis/Database/DataAccessObject/NextActivity/NextActivityDAOImpl.java
+++ Plugin/src/Analysis/Database/DataAccessObject/NextActivity/NextActivityDAOImpl.java
@@ -6,6 +6,7 @@ import Analysis.Database.QueryBuilder.QueryBuilder;
 import Analysis.Database.SQLiteOpenHelper;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by woong on 2016-02-29.
@@ -46,7 +47,7 @@ public class NextActivityDAOImpl extends SQLiteOpenHelper implements NextActivit
     }
 
     @Override
-    public NextActivityDTO select(String... col) {
+    public ArrayList<NextActivityDTO> select(String... col) {
         String query = null;
         if(col.length>0) query = QueryBuilder.selectAll().from(tableName).where(col[0]).build();
         else query = QueryBuilder.selectAll().from(tableName).build();
@@ -55,21 +56,24 @@ public class NextActivityDAOImpl extends SQLiteOpenHelper implements NextActivit
         PreparedStatement prep = null;
         Connection connection = getConnection();
         ResultSet rows = null;
-        NextActivityDTO nextActivityDTO = null;
+        ArrayList<NextActivityDTO> nextActivities = null;
         try{
-            prep = connection.prepareStatement(query);
+            prep = connection.prepareStatement(QueryBuilder.selectAll().from(tableName).where(query).build());
             rows = prep.executeQuery();
-            nextActivityDTO = new NextActivityDTO();
-            nextActivityDTO.setNum(rows.getInt(1));
-            nextActivityDTO.setJavaId(rows.getInt(2));
-            nextActivityDTO.setName(rows.getString(3));
-            nextActivityDTO.setIntentName(rows.getString(4));
-            nextActivityDTO.setIntentFuncName(rows.getString(5));
+            while(rows.next()) {
+                NextActivityDTO nextActivityDTO = new NextActivityDTO();
+                nextActivityDTO.setNum(rows.getInt(1));
+                nextActivityDTO.setJavaId(rows.getInt(2));
+                nextActivityDTO.setName(rows.getString(3));
+                nextActivityDTO.setIntentName(rows.getString(4));
+                nextActivityDTO.setIntentFuncName(rows.getString(5));
+                nextActivities.add(nextActivityDTO);
+            }
         }catch (SQLException e){
             e.printStackTrace();
         }finally {
             close(connection, prep, rows);
-            return nextActivityDTO;
+            return nextActivities;
         }
     }
 }

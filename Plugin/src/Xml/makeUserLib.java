1e9312a95a7413b1023b08835a633cc3749b9cd9
chojungho <scjh900@naver.com>
2016-03-03 21:33:41 +0900

    userLib 추가

---
+++ Plugin/src/Xml/makeUserLib.java
@@ -0,0 +1,46 @@
+package Xml;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+/**
+ * Created by cho on 2016-03-03.
+ */
+public class makeUserLib {
+
+
+
+    public makeUserLib(){
+
+    }
+
+
+    public static void makeXml(String xmlPath) {
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.newDocument();
+            Element resources =  doc.createElement("resource");
+            doc.appendChild(resources);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(xmlPath));
+            transformer.transform(source,result);
+
+        }catch (Exception e){
+
+        }
+    }
+}
